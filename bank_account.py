fbbb3db1da9e80e5b313f73a1406d465a8534f11
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-26 16:36:52 -0700

    add bank account class constructor

--- bank_account.py
+++ bank_account.py
@@ -7,6 +7,28 @@ import utl
 
 # //// CLASSES /////////////////////////////////////////////
 
+class BankAccount:
+    # don't forget to add some default values for these parameters!
+
+    def __init__(self, int_rate=10, balance=0):                 # Construct an instance of the bank account class
+        self.int_rate = int_rate
+        self.balance = balance
+        return self
+
+    def deposit(self, amount):                                  # Add amount to the bank account balance
+
+
+    def withdraw(self, amount):
+        pass
+        # your code here
+    def display_account_info(self):
+        pass
+        # your code here
+    def yield_interest(self):
+        pass
+        # your code here
+
 # //// FUNCTIONS ///////////////////////////////////////////
 
 # //// MAIN EXECUTABLE SECTION /////////////////////////////
+
