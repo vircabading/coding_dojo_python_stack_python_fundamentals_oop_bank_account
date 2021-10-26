7edc190486480ff14735fac5d836bec6d15806c1
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-26 16:41:34 -0700

    removed invalid nonprintable chars

--- bank_account.py
+++ bank_account.py
@@ -8,27 +8,25 @@ import utl
 # //// CLASSES /////////////////////////////////////////////
 
 class BankAccount:
-    # don't forget to add some default values for these parameters!
-
-    def __init__(self, int_rate=10, balance=0):                 # Construct an instance of the bank account class
+    def __init__(self, int_rate=10, balance=0):                 # Construct an instance of the bank account class
         self.int_rate = int_rate
         self.balance = balance
-        return self
 
-    def deposit(self, amount):                                  # Add amount to the bank account balance
+    def deposit(self, amount):                                  # Add amount to the bank account balance
+        pass
 
+    def withdraw(self, amount):
+        pass
+    
+    def display_account_info(self):
+        pass
 
-    def withdraw(self, amount):
+    def yield_interest(self):
         pass
-        # your code here
-    def display_account_info(self):
-        pass
-        # your code here
-    def yield_interest(self):
-        pass
-        # your code here
 
 # //// FUNCTIONS ///////////////////////////////////////////
 
 # //// MAIN EXECUTABLE SECTION /////////////////////////////
+
+account_1 = BankAccount(6,1000)
 
